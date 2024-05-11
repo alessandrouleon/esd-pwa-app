776ab29845930f91e67d68389df98573be18d3ca
alessandro uleon <alessandrouleon.dev@gmail.com>
2024-05-11 16:49:01 -0400

    feat: ajuste gera

--- src/services/history/history.ts
+++ src/services/history/history.ts
@@ -1,6 +1,5 @@
 const REGISTRATION = "@registration:esd";
 
-
 export const UserTokenHelper = {
   setLocalStorageRegistration: (registration: string) =>
     localStorage.setItem(REGISTRATION, registration),
